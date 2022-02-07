0e9da6f46d74fa463c622da9e35e5c17c0ea5e98
Irman Zaf <irman_zafyree_bin_irmanizam@s2019.ssts.edu.sg>
2022-02-07 08:14:37 +0800

    Going back to the old

---
+++ func.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env pybricks-micropython
+
+print("Starting import")
+
+import time
+
+from pybricks.ev3devices import (ColorSensor,Motor, TouchSensor)
+from pybricks.hubs import EV3Brick
+from pybricks.media.ev3dev import Font, ImageFile, SoundFile
+from pybricks.parameters import Button, Color, Direction, Port, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import DataLog, StopWatch, wait
+
+print("Done Importing, starting program")
+
+# Object Init
+big_font = Font(size=18, bold=True) # Fonts
+ev3 = EV3Brick() # EV3Brick
+last_error = 0
+
+ev3.speaker.set_volume(20, which='Beep')
+ev3.speaker.set_volume(100, which='PCM')
+ev3.speaker.set_speech_options(language='en', voice='m2', speed=180, pitch=50)
+# ev3.speaker.play_notes(['D#4/4', 'F4/4', 'G4/4', 'G#4/8', 'A#4/4', 'R/16',
+#                         'G#4/4', 'D#4/8', 'C#4/8', 'D#4/8', 'C#4/4', 'R/8',
+#                         'G4/4', 'D#4/8', 'C#4/8', 'D#4/8', 'C#4/4', 'R/8',
+#                         'B4/4', 'B4/8', 'B4/8', 'G#4/8', 'B4/8', 'C#5/8', 'B4/8', 'D#5/2', 'R/32',
+#                         'D#4/8', 'D#4/8', 'C#5/4', 'R/32',
+#                         'D#4/8', 'D#4/8', 'C#5/4', 'R/32',
+#                         'D#4/4', 'Db4/4', 'D#4/4', 'Db4/4', 'D#4/4', 'Db4/4', 'D#4/4', 'Db4/4',
+#                         'B4/8', 'Bb4/8', 'B4/8', 'Bb4/8', 'G#4/8', 'G#4/4', 'Gb4/8', 'G#4/4', 'R/4',
+#                         'G#4/4', 'Gb4/8', 'G#4/4', 'R/4',
+#                         'G#4/4', 'Gb4/8', 'G#4/4', 'R/4', ], tempo=120)
+
+
+# Setting Motors
+leftMotor = Motor(Port.B)
+rightMotor = Motor(Port.C)
+
+leftMedMotor = Motor(Port.D)
+rightMedMotor = Motor(Port.A)
+
+# Setting Sensors
+leftSensor = ColorSensor(Port.S1)
+middleSensor = ColorSensor(Port.S2)
+rightSensor = ColorSensor(Port.S3)
+buttonSensor = TouchSensor(Port.S4)
+
+# Creating Functions
+## Moving by given angle
+def MoveToAngle(leftAngle, rightAngle, leftSpeed, rightSpeed):
+    leftMotor.reset_angle(0)
+    rightMotor.reset_angle(0)
+    leftMotor.run(leftSpeed)
+    rightMotor.run(rightSpeed)
+    while (abs(leftMotor.angle()) < abs(leftAngle)) or (abs(rightMotor.angle()) < abs(rightAngle)):
+        if abs(rightMotor.angle()) >= abs(rightAngle):
+            rightMotor.hold()
+        if abs(leftMotor.angle()) >= abs(leftAngle):
+            leftMotor.hold()
+    leftMotor.hold()
+    rightMotor.hold()
+
+
+## Line Tracking
+def SinglePDTrack(sensorPort, threshold, kp, kd, speed):
+    if sensorPort == 1:
+        ref = leftSensor.reflection()
+        
+    elif sensorPort == 2:
+        ref = middleSensor.reflection()
+
+    else:
+        ref = rightSensor.reflection()
+
+    #print(ref)
+    global last_error  # im so smart
+    error = ref - threshold
+    p_gain = error * kp
+    derivative = error - last_error
+    d_gain = derivative * kd
+    #print(d_gain)
+    leftMotor.run(speed-(p_gain+d_gain))
+    rightMotor.run(speed+(p_gain+d_gain))
+    last_error = error
+
+## Line Tracking Till Junction
+def SinglePDTrackTillJunction(junctionPort, junctionThreshold, trackingPort, trackingThreshold, kp, kd, speed):
+    if junctionPort == 1:
+        while leftSensor.reflection() > junctionThreshold:
+            SinglePDTrack(trackingPort, trackingThreshold, kp, kd, speed)
+    elif junctionPort == 2:
+        while middleSensor.reflection() > junctionThreshold:
+            SinglePDTrack(trackingPort, trackingThreshold, kp, kd, speed)
+    else:
+        while rightSensor.reflection() > junctionThreshold:
+            SinglePDTrack(trackingPort, trackingThreshold, kp, kd, speed)
+    leftMotor.hold()
+    rightMotor.hold()
+
+## Line Tracking Till Degrees
+def SinglePDTrackTillDegrees(degrees, trackingPort, trackingThreshold, kp, kd, speed):
+    leftMotor.reset_angle(0)
+    while abs(leftMotor.angle()) < abs(degrees):
+        SinglePDTrack(trackingPort, trackingThreshold, kp, kd, speed)
+    leftMotor.stop()
+    rightMotor.stop()
+
+## Wait until it is pressed
+def WaitForPress():
+    while buttonSensor.pressed() == False:
+        pass
+    time.sleep(0.5)
+
+## Write on ev3 screen
+def WriteOnScreen(text):
+    print(text)
+    ev3.screen.set_font(big_font)
+    ev3.screen.clear()
+    ev3.screen.print(text)
+
+## Squaring on the line
+def SquareOnLine(Seconds, Target, Black, White, Approach, Forward, Backward):
+    while True:
+        while leftSensor.reflection() < White and rightSensor.reflection() < White:
+            leftMotor.run(Forward)
+            rightMotor.run(Forward)
+        while leftSensor.reflection() > Black and rightSensor.reflection() > Black:
+            leftMotor.run(Backward)
+            rightMotor.run(Backward)
+        if leftSensor.reflection() < Black:
+            leftMotor.hold()
+            while rightSensor.reflection() > Black:
+                rightMotor.run(Forward)
+            rightMotor.hold()
+            break
+        elif rightSensor.reflection() < Black:
+            rightMotor.hold()
+            while leftSensor.reflection() > Black:
+                leftMotor.run(Forward)
+            leftMotor.hold()
+            break
+    timeout = time.time() + Seconds
+    while True:
+        if leftSensor.reflection() < Target:
+            while leftSensor.reflection() < Target:
+                leftMotor.run(Backward)
+            leftMotor.hold()
+        elif leftSensor.reflection() > Target:
+            while leftSensor.reflection() > Target:
+                leftMotor.run(Approach)
+            leftMotor.hold()
+        if rightSensor.reflection() < Target:
+            while rightSensor.reflection() < Target:
+                rightMotor.run(Backward)
+            rightMotor.hold()
+        elif rightSensor.reflection() > Target:
+            while rightSensor.reflection() > Target:
+                rightMotor.run(Approach)
+            rightMotor.hold()
+        if time.time() > timeout:
+            break
+
---
+++ mainRun.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env pybricks-micropython
+
+from func import *
+
+def Run1():
+    WriteOnScreen("Run 1")
+    WaitForPress()
+    WriteOnScreen("Change Attachment")
+    rightMedMotor.run(-1200)
+    leftMedMotor.hold()
+    WaitForPress()
+    WriteOnScreen("Launching")
+    leftMedMotor.run(-500)
+    time.sleep(1)
+    leftMedMotor.hold()
+    rightMedMotor.hold()
+    
+    MoveToAngle(600, 612, -500, -510)
+    while leftSensor.reflection() > 15:
+        rightMotor.run(-300)
+        leftMotor.run(-300)
+    leftMotor.hold()
+    rightMotor.hold()
+    MoveToAngle(500, 510, -500, -510)
+    while leftSensor.reflection() > 15:
+        rightMotor.run(-300)
+        leftMotor.run(-300)
+    leftMotor.hold()
+    rightMotor.hold()
+    MoveToAngle(200, 204, -500, -510)
+    rightMedMotor.run(600)
+    time.sleep(1)
+    while leftSensor.reflection() > 15:
+        rightMotor.run(-300)
+        leftMotor.run(-300)
+    MoveToAngle(102, 100, -510, -500)
+    time.sleep(0.1)
+    MoveToAngle(1200, 1195, 500, 498)
+
+    
+    '''
+    SinglePDTrackTillJunction(2, 15, 3, 55, 2.7, 2.7, -370)
+    MoveToAngle(570, 570, -500, -500)
+    SinglePDTrackTillJunction(2, 15, 3, 55, 2.7, 2.7, -370)
+    MoveToAngle(570, 712, -400, -500)
+    SinglePDTrackTillDegrees(180, 3, 55, 2.7, 2.7, -370)
+    rightMedMotor.run(200)
+    sleep(1)
+    rightMedMotor.hold()
+    leftMedMotor.hold()
+    SinglePDTrackTillJunction(2, 15, 3, 55, 2.7, 2.7, -370)
+    MoveToAngle(100, 100, -500, -500)
+    '''
+
+Run1()
